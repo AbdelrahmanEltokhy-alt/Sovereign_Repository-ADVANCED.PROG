2ff556b491fde49b22df9862e3228923304a0d6e
aliahmedaliigcse <aliahmedaliigcse@gmail.com>
2026-04-26 00:29:03 +0200

    Inquiry system update
    + New inquiry repository/database implementation

--- sovereign/src/main/java/com/sovereign/repository/InquiryRepository.java
+++ sovereign/src/main/java/com/sovereign/repository/InquiryRepository.java
@@ -1,5 +1,9 @@
 package com.sovereign.repository;
 
-public class InquiryRepository {
-    
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sovereign.model.Inquiry;
+
+@Repository
+public interface InquiryRepository extends JpaRepository<Inquiry, Long> {}
--- sovereign/src/main/java/com/sovereign/service/DatabaseService.java
+++ sovereign/src/main/java/com/sovereign/service/DatabaseService.java
@@ -1,5 +1,39 @@
 package com.sovereign.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sovereign.model.Inquiry;
+import com.sovereign.repository.InquiryRepository;
+
+@Service
 public class DatabaseService {
-    
+
+    private ValidationService validator;
+    private final InquiryRepository inquiryRepo;
+
+    public DatabaseService(InquiryRepository inquiryRepo, ValidationService validator){
+        this.inquiryRepo = inquiryRepo;
+        this.validator = validator;
+    }
+
+    public void addInquiry(Inquiry inquiry){
+
+        if(!validator.validateInquiry(inquiry)){
+            throw new RuntimeException("All fields must be filled.");
+        }
+
+        inquiryRepo.save(inquiry);
+
+    }
+
+    public List<Inquiry> listInquiries(){
+        return inquiryRepo.findAll();
+    }
+
+    public void deleteInquiry(Long id){
+        inquiryRepo.deleteById(id);
+    }
+
 }
