f64baaad71a189dc278f95a53648c46e176ae539
Abdelrahman Eltokhy <abdelrhmn.eltokhy@gmail.com>
2026-04-28 23:31:57 +0300

    User repo created

---
+++ sovereign/src/main/java/com/sovereign/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.sovereign.repository;
+
+import com.sovereign.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+
+@Repository
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    
+}
