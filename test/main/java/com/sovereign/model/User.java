9f2c11fef1c194ce415aa8a385a4fbb95e9d8a3b
Eng / Omar Fouad <omar.a.m.fouad@gmail.com>
2026-05-10 17:25:21 +0300

    Refactor User class to remove role field
    Removed Role field and updated constructor and methods accordingly.  & the previous code have many issues

--- test/main/java/com/sovereign/model/User.java
+++ test/main/java/com/sovereign/model/User.java
@@ -10,46 +10,32 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int  id;
+    private int id;
 
     private String username;
     private String email;
     private String pass;
-    private String role;
 
     public User() {}
 
-    public User(int id, String name, String email, String pass,String role) {
-        super();
-        this.id =id; 
-        this.username = name;
+    public User(int id, String username, String email, String pass) {
+        this.id = id;
+        this.username = username;
         this.email = email;
         this.pass = pass;
-        this.role= role ;
     }
 
-    public long getId() { return id; }
+    public int getId() { return id;}
+    public void setId(int id) { this.id = id;}
 
-    public void setId(int id) { this.id = id; }
-
-    public String getusername() { return username; }
-
-    public void setusername(String username) { this.username = username; }
-
-    public String getEmail() { return email; }
-
-    public void setEmail(String email) { this.email = email; }
-
-    public String getPass() { return pass; }
-
-    public void setPass(String pass) { this.pass = pass; }
-
-	public String getRole() {
-		return role;
-	}
-
-	public void setRole(String role) {
-		this.role = role;
-	}
     
-}
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username;}
+
+    
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+
+    
+    public String getPass() { return pass;}
+    public void setPass(String pass) { this.pass = pass;}
