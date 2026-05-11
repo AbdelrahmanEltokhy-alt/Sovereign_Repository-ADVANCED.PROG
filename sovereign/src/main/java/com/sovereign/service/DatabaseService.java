02d4cf310a0d891e933b5ed7cb8389e1fb265fbf
aliahmedaliigcse <aliahmedaliigcse@gmail.com>
2026-05-11 16:36:53 +0200

    Session system update
    - Removed redundant files to clean up branch

--- sovereign/src/main/java/com/sovereign/service/DatabaseService.java
+++ sovereign/src/main/java/com/sovereign/service/DatabaseService.java
@@ -5,43 +5,20 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.sovereign.model.Car;
-import com.sovereign.model.Inquiry;
 import com.sovereign.repository.CarRepository;
-import com.sovereign.repository.InquiryRepository;
 
 @Service
 public class DatabaseService {
 
-    private ValidationService validator;
-    private final InquiryRepository inquiryRepo;
     private final CarRepository carRepo;
 
-    public DatabaseService(InquiryRepository inquiryRepo, CarRepository carRepo, ValidationService validator){
-        this.inquiryRepo = inquiryRepo;
+    public DatabaseService(CarRepository carRepo){
         this.carRepo = carRepo;
-        this.validator = validator;
     }
 
-    public void addInquiry(Inquiry inquiry){
-
-        if(!validator.validateInquiry(inquiry)){
-            throw new RuntimeException("All fields must be filled.");
-        }
-
-        inquiryRepo.save(inquiry);
-
-    }
-
-    public List<Inquiry> listInquiries(){
-        return inquiryRepo.findAll();
-    }
 
     public Car findCarById(Long id){
         return carRepo.findById(id).orElse(null);
-    }
-
-    public void deleteInquiry(Long id){
-        inquiryRepo.deleteById(id);
     }
 
 }
